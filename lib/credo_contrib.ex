a1c52a22871feab3a1d89683c9380d5cd60b4c58
Christian Wesselhoeft <hi@xtian.us>
2018-01-19 16:45:22 -0700

    Initial commit

---
+++ lib/credo_contrib.ex
@@ -0,0 +1,18 @@
+defmodule CredoContrib do
+  @moduledoc """
+  Documentation for CredoContrib.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CredoContrib.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/credo_contrib_test.exs
@@ -0,0 +1,8 @@
+defmodule CredoContribTest do
+  use ExUnit.Case
+  doctest CredoContrib
+
+  test "greets the world" do
+    assert CredoContrib.hello() == :world
+  end
+end
