fe0e40a3ed5f5f14026ec3f8d0fae0e8530d3cbb
Christian Wesselhoeft <hi@xtian.us>
2018-02-09 09:26:23 -0700

    MixProject: Use RC version since Credo 0.9 is still an RC

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CredoContrib.MixProject do
   def project do
     [
       app: :credo_contrib,
-      version: "0.1.0",
+      version: "0.1.0-rc1",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
