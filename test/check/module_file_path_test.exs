d719db6fccb38177de1ed7c3dc35eb55f0a67086
Christian Wesselhoeft <cwesselhoeft@enbala.com>
2018-03-30 16:20:21 -0600

    workin

--- test/check/module_file_path_test.exs
+++ test/check/module_file_path_test.exs
@@ -3,147 +3,50 @@ defmodule CredoContrib.Check.ModuleFilePathTest do
 
   @described_check CredoContrib.Check.ModuleFilePath
 
-  test "does not report for conventional module name" do
-    """
-    defmodule SomeLib.Foo.Bar do
+  @valid_module_names %{
+    "apps/some_child/lib/controllers/foo_bar_controller.ex" => "SomeChild.FooBarController",
+    "apps/some_child/lib/controller/foo/bar_controller.ex" => "SomeChild.Foo.BarController",
+    "apps/some_child/lib/foo_bar.ex" => "SomeChild.FooBar",
+    "apps/some_child/test/controllers/foo_bar_controller_test.exs" =>
+      "SomeChild.FooBarControllerTest",
+    "apps/some_child/test/foo_bar_test.exs" => "SomeChild.FooBarTest",
+    "apps/some_child/test/support/test_utils.ex" => "SomeChild.TestUtils",
+    "lib/controllers/foo_bar_controller.ex" => "SomeLib.FooBarController",
+    "lib/controllers/foo/bar_controller.ex" => "SomeLib.Foo.BarController",
+    "lib/credo_contrib/foo/bar.ex" => "CredoContrib.Foo.Bar",
+    "lib/foo/bar.ex" => "SomeLib.Foo.Bar",
+    "test/controllers/foo_bar_controller_test.exs" => "SomeLib.FooBarControllerTest",
+    "test/foo_bar_test.exs" => "SomeLib.FooBarTest",
+    "test/support/test_utils.ex" => "SomeLib.TestUtils",
+    "web/controllers/foo_bar_controller_test.exs" => "SomeLib.FooBarControllerTest"
+  }
+
+  @invalid_module_names %{
+    "apps/some_child/lib/some_child/foo_bar.ex" => "SomeChild.FooBar",
+    "apps/some_child/test/foo_bar_test.exs" => "SomeOtherChild.FooBarTest",
+    "lib/foo/bar.ex" => "SomeLib.Foo.Baz",
+    "test/foo_bar_test.exs" => "SomeLib.FooBazTest"
+  }
+
+  test "does not report issue for conventional module names" do
+    for {path, valid_module_name} <- @valid_module_names do
+      """
+      defmodule #{valid_module_name} do
+      end
+      """
+      |> to_source_file(path)
+      |> refute_issues(@described_check)
     end
-    """
-    |> to_source_file("lib/foo/bar.ex")
-    |> refute_issues(@described_check)
   end
 
-  test "does not report for module name using plural convention" do
-    """
-    defmodule SomeLib.FooBarController do
+  test "reports issue for unconventional module names" do
+    for {path, invalid_module_name} <- @invalid_module_names do
+      """
+      defmodule #{invalid_module_name} do
+      end
+      """
+      |> to_source_file(path)
+      |> assert_issue(@described_check)
     end
-    """
-    |> to_source_file("lib/controllers/foo_bar_controller.ex")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report for conventional module name in umbrella" do
-    """
-    defmodule SomeChild.FooBar do
-    end
-    """
-    |> to_source_file("apps/some_child/lib/foo_bar.ex")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report for top-level module as lib directory" do
-    """
-    defmodule CredoContrib.Foo.Bar do
-    end
-    """
-    |> to_source_file("lib/credo_contrib/foo/bar.ex")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report for module name using plural convention in umbrella" do
-    """
-    defmodule SomeChild.FooBarController do
-    end
-    """
-    |> to_source_file("apps/some_child/lib/controllers/foo_bar_controller.ex")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report issue for conventional test support module name" do
-    """
-    defmodule SomeLib.TestUtils do
-    end
-    """
-    |> to_source_file("test/support/test_utils.ex")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report issue for conventional test support module name in umbrella" do
-    """
-    defmodule SomeChild.TestUtils do
-    end
-    """
-    |> to_source_file("apps/some_child/test/support/test_utils.ex")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report issue for conventional test module name" do
-    """
-    defmodule SomeLib.FooBarTest do
-    end
-    """
-    |> to_source_file("test/foo_bar_test.exs")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report for web module name using plural convention" do
-    """
-    defmodule SomeLib.FooBarControllerTest do
-    end
-    """
-    |> to_source_file("web/controllers/foo_bar_controller_test.exs")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report for test module name using plural convention" do
-    """
-    defmodule SomeLib.FooBarControllerTest do
-    end
-    """
-    |> to_source_file("test/controllers/foo_bar_controller_test.exs")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report issue for conventional test module name in umbrella" do
-    """
-    defmodule SomeChild.FooBarTest do
-    end
-    """
-    |> to_source_file("apps/some_child/test/foo_bar_test.exs")
-    |> refute_issues(@described_check)
-  end
-
-  test "does not report for test module name using plural convention in umbrella" do
-    """
-    defmodule SomeChild.FooBarControllerTest do
-    end
-    """
-    |> to_source_file("apps/some_child/test/controllers/foo_bar_controller_test.exs")
-    |> refute_issues(@described_check)
-  end
-
-  test "reports issue for unconventional module name" do
-    """
-    defmodule SomeLib.Foo.Baz do
-    end
-    """
-    |> to_source_file("lib/foo/bar.ex")
-    |> assert_issue(@described_check)
-  end
-
-  test "reports issue for conventional module name in umbrella" do
-    """
-    defmodule SomeChild.FooBar do
-    end
-    """
-    |> to_source_file("apps/some_child/lib/some_child/foo_bar.ex")
-    |> assert_issue(@described_check)
-  end
-
-  test "reports issue for unconventional test module name" do
-    """
-    defmodule SomeLib.FooBazTest do
-    end
-    """
-    |> to_source_file("test/foo_bar_test.exs")
-    |> assert_issue(@described_check)
-  end
-
-  test "reports issue for unconventional test module name in umbrella" do
-    """
-    defmodule SomeOtherChild.FooBarTest do
-    end
-    """
-    |> to_source_file("apps/some_child/test/foo_bar_test.exs")
-    |> assert_issue(@described_check)
   end
 end
